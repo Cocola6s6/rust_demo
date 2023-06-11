e05f6dc7ab54146b9eb40ac2889bd6d8b4d4c9d0
Cocola6s6 <799497008@qq.com>
2023-06-12 00:04:58 +0800

    add: 添加webcam_live

---
+++ webcam_live/src/lib.rs
@@ -0,0 +1,38 @@
+use tracing::info;
+use wasm_bindgen::{JsValue, JsCast};
+use wasm_bindgen_futures::JsFuture;
+use web_sys::{HtmlVideoElement, MediaStreamConstraints, MediaStream};
+
+pub struct VideoStream {
+    el: HtmlVideoElement,
+}
+
+impl VideoStream {
+    pub fn new(el: HtmlVideoElement) -> VideoStream {
+        VideoStream {
+            el: el,
+        }
+    }
+
+    pub async fn set_video_src(&self, video_constraints: &serde_json::Value) -> () {
+        let window = web_sys::window().expect("no windows");
+        let navigator = window.navigator();
+        let devices = navigator
+        .media_devices()
+        .expect("no navigator.device");
+        
+        web_sys::console::log_1(&devices);
+
+        let mut constraints = MediaStreamConstraints::new();
+        constraints.video(&JsValue::from_serde(video_constraints).unwrap());
+        constraints.audio(&false.into());
+        let media = JsFuture::from(
+            devices
+            .get_user_media_with_constraints(&constraints)
+            .unwrap(),
+        ).await.unwrap();
+        let media_stream = media.unchecked_into::<MediaStream>();
+        info!("media_stream: {:?}", media_stream);
+        self.el.set_src_object(Some(&media_stream));
+    }
+}
---
+++ webcam_live/src/main.rs
@@ -0,0 +1,70 @@
+use sycamore::prelude::*;
+use tracing::info;
+use web_sys::*;
+use webcam_live::VideoStream;
+fn main() {
+    println!("Hello, world!");
+
+    console_error_panic_hook::set_once();
+    tracing_wasm::set_as_global_default();
+
+    sycamore::render(|ctx| {
+        view! {ctx,
+            // test1
+            p {
+                "Hello, World!"
+            }
+
+            // test2
+            div(class="container p-2") {
+                div(class="row") {
+                    div(class= "col-12") {
+                        h1(class= "text-center hero-text text-blue font-bold") {
+                            "Hello World!"
+                        }
+                    }
+                }
+            }
+
+            // test3
+            div(class="container p-2") {
+                Video()
+            }
+        }
+    })
+}
+
+#[component]
+fn Video<G: Html>(ctx: Scope) -> View<G> {
+    let video_ref = create_node_ref(ctx);
+
+    let video_future = async move {
+        let el = video_ref
+            .get::<DomNode>()
+            .unchecked_into::<HtmlVideoElement>();
+        let video_stream = VideoStream::new(el);
+        video_stream.set_video_src(&serde_json::json! ({
+            "audio": false,
+            "video": {
+                "facingMode": "environment",
+                "width": "640",
+                "heidht":"480",
+            }
+        }));
+    };
+
+    wasm_bindgen_futures::spawn_local(video_future);
+
+    info!("test===============");
+    view! {ctx,
+        div {
+            video(
+                ref=video_ref,
+                class="border border-gray-400 rounded-lg",
+                autoplay=true,
+                width=640,
+                height=480)
+                // src="https://samplelib.com/lib/preview/mp4/sample-5s.mp4")
+        }
+    }
+}
